36fce3e00e4e65ccdc82016a1fcc2b34a3c3f870
meloarteaga <73600799+meloarteaga@users.noreply.github.com>
2021-09-26 20:55:10 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/MiMICLogo.png">
+  <HelloWorld msg="Hello World!"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+  background: #aecfff;
+}
+</style>
